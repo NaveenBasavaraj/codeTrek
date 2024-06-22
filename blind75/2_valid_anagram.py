ca4aad8b6cd880c3f32bf2a95cf980e5e0db6cfd
Naveen Basavaraj <naveen.b.workmail@gmail.com>
2024-06-22 13:09:14 +0000

    anagram

---
+++ blind75/2_valid_anagram.py
@@ -0,0 +1,21 @@
+
+class Anagram:
+    def is_anagram(self, s:str, t:str) -> bool:
+        '''
+        Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+
+        An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
+        Example 1:
+            Input: s = "racecar", t = "carrace"
+            Output: true
+
+        Example 2:
+            Input: s = "jar", t = "jam"
+            Output: false
+
+        '''
+        if len(s) != len(t):
+            return False
+        
+
